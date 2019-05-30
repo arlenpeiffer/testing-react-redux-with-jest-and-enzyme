aee93524f03e224b5e9476481f38bef589611bad
Pepsi Cola <>
2019-05-29 21:27:11 -0700

    simulate click event on button component

--- src/components/button/Button.test.js
+++ src/components/button/Button.test.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { checkProps, findByTestAttr } from "./../../../Utils";
-import Button from "./index";
+import React from 'react';
+import { shallow } from 'enzyme';
+import { checkProps, findByTestAttr } from './../../../Utils';
+import Button from './index';
 
-describe("Button Component", () => {
-  describe("Checking PropTypes", () => {
-    it("Should not throw a warning", () => {
+describe('Button Component', () => {
+  describe('Checking PropTypes', () => {
+    it('Should not throw a warning', () => {
       const expectedProps = {
-        buttonText: "Test Button Text",
+        buttonText: 'Test Button Text',
         emitEvent: () => {}
       };
       const propsErr = checkProps(Button, expectedProps);
@@ -15,19 +15,28 @@ describe("Button Component", () => {
     });
   });
 
-  describe("Renders", () => {
+  describe('Renders', () => {
     let wrapper;
+    let mockFunc;
     beforeEach(() => {
+      mockFunc = jest.fn();
       const props = {
-        buttonText: "Test Button Text",
-        emitEvent: () => {}
+        buttonText: 'Test Button Text',
+        emitEvent: mockFunc
       };
       wrapper = shallow(<Button {...props} />);
     });
 
-    it("Should render without errors", () => {
-      const component = findByTestAttr(wrapper, "buttonComponent");
+    it('Should render without errors', () => {
+      const component = findByTestAttr(wrapper, 'buttonComponent');
       expect(component.length).toBe(1);
+    });
+
+    it('Should emit callback on click event', () => {
+      const component = findByTestAttr(wrapper, 'buttonComponent');
+      component.simulate('click');
+      const callback = mockFunc.mock.calls.length;
+      expect(callback).toBe(1);
     });
   });
 });
