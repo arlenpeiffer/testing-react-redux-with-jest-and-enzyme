e5ad07f4d6532e42dbaf46e03a6bc4385356f6b0
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-06-03 16:31:58 -0700

    Testing methods on our class

--- src/App.test.js
+++ src/App.test.js
@@ -37,4 +37,17 @@ describe('App Component', () => {
     const component = findByTestAttr(wrapper, 'appComponent');
     expect(component.length).toBe(1);
   });
+
+  it('exampleMethod_updatesState should update state as expected', () => {
+    const classInstance = wrapper.instance();
+    classInstance.exampleMethod_updatesState();
+    const newState = classInstance.state.hideBtn;
+    expect(newState).toBe(true);
+  });
+
+  it('exampleMethod_returnsValue should return value as expected', () => {
+    const classInstance = wrapper.instance();
+    const newValue = classInstance.exampleMethod_returnsValue(6);
+    expect(newValue).toBe(7);
+  });
 });
