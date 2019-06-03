f075533b4fd1fa118884a75d591eee5672dbfd03
Pepsi Cola <>
2019-06-03 16:31:58 -0700

    Testing methods on our class

--- src/App.js
+++ src/App.js
@@ -17,18 +17,36 @@ const tempArr = [
   }
 ];
 
+const initialState = {
+  hideBtn: false
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = { ...initialState };
     this.fetch = this.fetch.bind(this);
   }
 
   fetch() {
     this.props.fetchPosts();
+    this.exampleMethod_updatesState();
+  }
+
+  exampleMethod_updatesState() {
+    const { hideBtn } = this.state;
+    this.setState({
+      hideBtn: !hideBtn
+    });
+  }
+
+  exampleMethod_returnsValue(number) {
+    return number + 1;
   }
 
   render() {
     const { posts } = this.props;
+    const { hideBtn } = this.state;
 
     const configButton = {
       buttonText: 'Get posts',
@@ -44,7 +62,9 @@ class App extends Component {
             desc="Click the button to render posts!"
             tempArr={tempArr}
           />
-          <Button {...configButton} />
+
+          {!hideBtn && <Button {...configButton} />}
+
           {posts.length > 0 && (
             <div>
               {posts.map((post, index) => {
